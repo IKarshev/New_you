d0201f469422805be6f7385a24f7a0a346d7310e
IKarshev <karshev.ivan09.07.38@gmail.com>
2022-10-11 14:11:15 +0700

    update price list
    Обновил прайс лист

--- price_list.php
+++ price_list.php
@@ -1,74 +1,290 @@
 <div class="item_container">
 
-<div class="item left">
-    <div class="heading">
-        <h1>Массаж</h1>
+
+    <div class="item left">
+        <div class="heading">
+            <h1>Массаж детский</h1>
+        </div>
+        <ul>
+            <li>
+                <div class="title">С 1 месяца до 3 лет</div>
+                <div class="price">600р</div>
+            </li>
+
+            <li>
+                <div class="title">С 3 лет до 7 лет</div>
+                <div class="price">700р</div>
+            </li>
+
+            <li>
+                <div class="title">С 7 лет до 14 лет</div>
+                <div class="price">750р</div>
+            </li>
+
+        </ul>
     </div>
-    <ul>
-        <li>
-            <div class="title">Массаж спины</div>
-            <div class="price">600р</div>
-        </li>
-        <li>
-            <div class="title">Общий массаж</div>
-            <div class="price">1200р</div>
-        </li>
-        <li>
-            <div class="title">Моделирующий массаж лица</div>
-            <div class="price">600р</div>
-        </li>
-        <li>
-            <div class="title">Лимфодренажный</div>
-            <div class="price">1200р</div>
-        </li>
-        <li>
-            <div class="title">Массаж головы</div>
-            <div class="price">600р</div>
-        </li>
-        <li>
-            <div class="title">Детский массаж (от 0 до 7 лет)</div>
-            <div class="price">600р</div>
-        </li>
-        <li>
-            <div class="title">Детский массаж (от 7 до 14)</div>
-            <div class="price">700р</div>
-        </li>
-    </ul>
+
+    <div class="item right">
+        <div class="heading">
+            <h1>Массаж</h1>
+        </div>
+        <ul>
+            <li>
+                <div class="title">Массаж спины</div>
+                <div class="price">600р</div>
+            </li>
+            <li>
+                <div class="title">Общий массаж</div>
+                <div class="price">1200р</div>
+            </li>
+            <li>
+                <div class="title">Моделирующий массаж лица</div>
+                <div class="price">600р</div>
+            </li>
+            <li>
+                <div class="title">Лимфодренажный массаж</div>
+                <div class="price">1200р</div>
+            </li>
+            <li>
+                <div class="title">Массаж головы</div>
+                <div class="price">600р</div>
+            </li>
+            <li>
+                <div class="title">Антицеллюлитный (живот, бока) -30мин </div>
+                <div class="price">600р</div>
+            </li>
+            <li>
+                <div class="title">Антицеллюлитный (бедра, ягодицы) -30мин </div>
+                <div class="price">600р</div>
+            </li>
+
+        </ul>
+    </div>
 </div>
 
-<div class="item right">
-    <div class="heading">
-        <h1>Массаж</h1>
+<div class="item_container">
+
+    <div class="item left">
+        <div class="heading">
+            <h1>Массаж аппаратный</h1>
+        </div>
+        <ul>
+            <li>
+                <div class="title">Ролико-вакуумный -30 мин</div>
+                <div class="price">600р</div>
+            </li>
+
+            <li>
+                <div class="title">Вакуум + RF-лифтинг -30 мин</div>
+                <div class="price">750р</div>
+            </li>
+
+            <li>
+                <div class="title">Кавитация -30 мин</div>
+                <div class="price">600р</div>
+            </li>
+
+            <li>
+                <div class="title">RF-лифтинг тела (1 зона) -30 мин</div>
+                <div class="price">700р</div>
+            </li>
+
+
+        </ul>
     </div>
-    <ul>
-        <li>
-            <div class="title">Массаж спины</div>
-            <div class="price">600р</div>
-        </li>
-        <li>
-            <div class="title">Общий массаж</div>
-            <div class="price">1200р</div>
-        </li>
-        <li>
-            <div class="title">Моделирующий массаж лица</div>
-            <div class="price">600р</div>
-        </li>
-        <li>
-            <div class="title">Лимфодренажный</div>
-            <div class="price">1200р</div>
-        </li>
-        <li>
-            <div class="title">Массаж головы</div>
-            <div class="price">600р</div>
-        </li>
-        <li>
-            <div class="title">Детский массаж (от 0 до 7 лет)</div>
-            <div class="price">600р</div>
-        </li>
-        <li>
-            <div class="title">Детский массаж (от 7 до 14)</div>
-            <div class="price">700р</div>
-        </li>
-    </ul>
+
+    <div class="item right">
+        <div class="heading">
+            <h1>Шугаринг</h1>
+        </div>
+        <ul>
+            <li>
+                <div class="title">Подмышечные впадины</div>
+                <div class="price">400р</div>
+            </li>
+            <li>
+                <div class="title">Ноги до колен</div>
+                <div class="price">600р</div>
+            </li>
+            <li>
+                <div class="title">Ноги полностью</div>
+                <div class="price">800р</div>
+            </li>
+            <li>
+                <div class="title">Руки</div>
+                <div class="price">600р</div>
+            </li>
+            <li>
+                <div class="title">Бикини классика</div>
+                <div class="price">600р</div>
+            </li>
+            <li>
+                <div class="title">Бикини глубокое</div>
+                <div class="price">900р</div>
+            </li>
+
+        </ul>
+    </div>
 </div>
+
+<div class="item_container">
+
+    <div class="item left">
+        <div class="heading">
+            <h1>Косметологические услуги</h1>
+        </div>
+        <ul>
+            <li>
+                <div class="title">Чистка лица(УЗ+механическая)</div>
+                <div class="price">1500р</div>
+            </li>
+            <li>
+                <div class="title">Маска</div>
+                <div class="price">от 300р</div>
+            </li>
+            <li>
+                <div class="title">Пилинг</div>
+                <div class="price">от 300р</div>
+            </li>
+            <li>
+                <div class="title">Мезотерапия</div>
+                <div class="price">от 1500р</div>
+            </li>
+            <li>
+                <div class="title">Буккальный+миофасциальный массаж лица</div>
+                <div class="price">700р</div>
+            </li>
+            <li>
+                <div class="title">Дарсонваль</div>
+                <div class="price">400р</div>
+            </li>
+            <li>
+                <div class="title">Гальванизация</div>
+                <div class="price">400р</div>
+            </li>
+            <li>
+                <div class="title">RF-лифтинг лица</div>
+                <div class="price">700р</div>
+            </li>
+            <li>
+                <div class="title">Антицеллюлитные программы (липолитики)</div>
+                <div class="price">от 1500р</div>
+            </li>
+            <li>
+                <div class="title">Консультация косметолога</div>
+                <div class="price">БЕСПЛАТНО</div>
+            </li>
+            <li>
+                <div class="title">Лечение акне</div>
+                <div class="price">1600р</div>
+            </li>
+            <li>
+                <div class="title">Уходовая процедура "Лицо на выход"</div>
+                <div class="price">2000р</div>
+            </li>
+
+
+        </ul>
+    </div>
+    <div class="item right">
+        <div class="heading">
+            <h1>Наращивание ресниц</h1>
+        </div>
+        <ul>
+            <li>
+                <div class="title">Классика</div>
+                <div class="price">1500р</div>
+            </li>
+            <li>
+                <div class="title">2d</div>
+                <div class="price">1800</div>
+            </li>
+            <li>
+                <div class="title">3d-4d</div>
+                <div class="price">2000</div>
+            </li>
+            <li>
+                <div class="title">5d-6d</div>
+                <div class="price">2100</div>
+            </li>
+
+
+        </ul>
+    </div>
+
+</div>
+
+<div class="item_container">
+
+    <div class="item left">
+        <div class="heading">
+            <h1>Оформление бровей</h1>
+        </div>
+        <ul>
+            <li>
+                <div class="title">Коррекция</div>
+                <div class="price">150р</div>
+            </li>
+            <li>
+                <div class="title">Окрашивание краской</div>
+                <div class="price">200р</div>
+            </li>
+            <li>
+                <div class="title">Окрашивание хной</div>
+                <div class="price">250р</div>
+            </li>
+            <li>
+                <div class="title">Ламинирование</div>
+                <div class="price">600р</div>
+            </li>
+
+        </ul>
+    </div>
+
+    <div class="item left">
+        <div class="heading">
+            <h1>Ногтевой сервис</h1>
+        </div>
+        <ul>
+            <li>
+                <div class="title">Маникюр без покрытия</div>
+                <div class="price">500р</div>
+            </li>
+
+            <li>
+                <div class="title">Маникюр+покрытие гель-лак в один тон</div>
+                <div class="price">1000р</div>
+            </li>
+            <li>
+                <div class="title">Маникюр+покрытие гель-лак "кошачий глаз"</div>
+                <div class="price">1100р</div>
+            </li>
+            <li>
+                <div class="title">Ремонт 1 ноготок</div>
+                <div class="price">+50р</div>
+            </li>
+            <li>
+                <div class="title">Укрепление ногтевой пластины (акригель)</div>
+                <div class="price">+200р</div>
+            </li>
+            <li>
+                <div class="title">Поднятие клюющих ноготков</div>
+                <div class="price">+300р</div>
+            </li>
+            <li>
+                <div class="title">Френч</div>
+                <div class="price">+300р</div>
+            </li>
+            <li>
+                <div class="title">Дизайн на все ноготки</div>
+                <div class="price">+300р</div>
+            </li>
+            <li>
+                <div class="title">Наращивание ногтей</div>
+                <div class="price">1500р</div>
+            </li>
+
+        </ul>
+    </div>
+
 </div>
