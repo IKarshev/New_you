295b5088cdb0ddab492de5a3d886f99ff95c51df
IKarshev <karshev.ivan09.07.38@gmail.com>
2022-10-28 21:35:15 +0700

    Добавил несколько слайдов и фото к ним

---
+++ slides.php
@@ -0,0 +1,59 @@
+<div class="slider__item">
+    <div class="slide_cont">
+        <div class="img_cont"><img src="./media/slider_imgs/3.jpg" alt=""></div>
+        <div class="text_cont">
+            <h1>Наращивание ресниц.</h1>
+            <p>С помощью разнообразных техник можно корректировать форму глаза и подчеркнуть достоинства.</p>
+        </div>
+    </div>
+</div>
+
+<div class="slider__item">
+    <div class="slide_cont">
+        <div class="img_cont"><img src="./media/slider_imgs/2.jpeg" alt=""></div>
+        <div class="text_cont">
+            <h1>Массаж</h1>
+            <p>Предоставляем детский и взрослый профессиональный массаж.</p>
+        </div>
+    </div>
+</div>
+
+<div class="slider__item">
+    <div class="slide_cont">
+        <div class="img_cont"><img src="./media/slider_imgs/1.jpeg" alt=""></div>
+        <div class="text_cont">
+            <h1>Косметология</h1>
+            <p>Комбинированный подход-аппаратной и эстетической косметологии,принесет вам максимально желаемый результат.</p>
+        </div>
+    </div>
+</div>
+
+<div class="slider__item">
+    <div class="slide_cont">
+        <div class="img_cont"><img src="" alt=""></div>
+        <div class="text_cont">
+            <h1>Депиляция</h1>
+            <p>Гладкая и чистая кожа на лице и теле является символом женственности,признаком ухоженности.</p>
+        </div>
+    </div>
+</div>
+
+<div class="slider__item">
+    <div class="slide_cont">
+        <div class="img_cont"><img src="" alt=""></div>
+        <div class="text_cont">
+            <h1>Ногтевой сервис</h1>
+            <p>Красивые ухоженные ногти-залог уверенности в себе и высокой самооценки.</p>
+        </div>
+    </div>
+</div>
+
+<div class="slider__item">
+    <div class="slide_cont">
+        <div class="img_cont"><img src="" alt=""></div>
+        <div class="text_cont">
+            <h1>Оформление бровей</h1>
+            <p>Художественно оформленые брови делают лицо гармоничным и привлекательным.</p>
+        </div>
+    </div>
+</div>
