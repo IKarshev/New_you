a7376dda38e6d17ac5c680547e0a194ce67c59f3
IKarshev <karshev.ivan09.07.38@gmail.com>
2022-10-30 17:18:36 +0700

    добавил оставшиеся фото в слайдер

--- slides.php
+++ slides.php
@@ -30,7 +30,7 @@
 
 <div class="slider__item">
     <div class="slide_cont">
-        <div class="img_cont"><img src="" alt=""></div>
+        <div class="img_cont"><img src="./media/slider_imgs/4.jpg" alt=""></div>
         <div class="text_cont">
             <h1>Депиляция</h1>
             <p>Гладкая и чистая кожа на лице и теле является символом женственности,признаком ухоженности.</p>
@@ -40,7 +40,7 @@
 
 <div class="slider__item">
     <div class="slide_cont">
-        <div class="img_cont"><img src="" alt=""></div>
+        <div class="img_cont"><img src="./media/slider_imgs/6.jpg" alt=""></div>
         <div class="text_cont">
             <h1>Ногтевой сервис</h1>
             <p>Красивые ухоженные ногти-залог уверенности в себе и высокой самооценки.</p>
@@ -50,7 +50,7 @@
 
 <div class="slider__item">
     <div class="slide_cont">
-        <div class="img_cont"><img src="" alt=""></div>
+        <div class="img_cont"><img src="./media/slider_imgs/5.jpg" alt=""></div>
         <div class="text_cont">
             <h1>Оформление бровей</h1>
             <p>Художественно оформленые брови делают лицо гармоничным и привлекательным.</p>
