50e5502d1a1db2cd25352c215306340fb03132da
IKarshev <karshev.ivan09.07.38@gmail.com>
2022-11-04 22:22:01 +0700

    Создал страницу авторизации, настроил SITE_DEFAULT_PATH 2

---
+++ pages/personal_account/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Регистрация/Авторизация</title>
+    <?include($_SERVER["DOCUMENT_ROOT"]."/functions.php");?>
+    <script src="<?=SITE_DEFAULT_PATH."/js/lib/jquery.js";?>"></script>
+    <link rel="stylesheet" href="<?=SITE_DEFAULT_PATH."/pages/personal_account/personal_account.css";?>">
+</head>
+<body>
+
+</body>
+<script src="<?=SITE_DEFAULT_PATH."/pages/personal_account/personal_account.js";?>"></script>
+</html>
